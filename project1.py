1b9d96fff7d4cf8a05babd0a7fb895b3030efc1b
April Xiao <aprilx@umich.edu>
2025-10-07 13:36:29 -0400

    added def read_penguin_data and imported csv/math

--- project1.py
+++ project1.py
@@ -2,3 +2,14 @@
 ### Email: aprilx@umich.edu
 ### uniqname: apr1lx
 ### Course: Fall 2025 - SI 201
+import csv 
+import math 
+
+def read_penguin_data(csv_file):
+    data = []
+    with open(csv_file, mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    print(data)
+    return data
