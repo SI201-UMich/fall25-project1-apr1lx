09880b70f94111c677392b8aac7dccb8bab050f6
April Xiao <aprilx@umich.edu>
2025-10-07 13:44:35 -0400

    added def filter_by_species helper function

--- project1.py
+++ project1.py
@@ -4,6 +4,7 @@
 ### Course: Fall 2025 - SI 201
 import csv 
 import math 
+csv_path = 'penguins.csv'
 
 def read_penguin_data(csv_file):
     data = []
@@ -11,5 +12,15 @@ def read_penguin_data(csv_file):
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
-    print(data)
+    #print(data)
     return data
+
+def filter_by_species(data, species):
+    filtered = []
+    for row in data:
+        if row.get("species") == species:
+            filtered.append(row)
+    return filtered
+
+
+read_penguin_data(csv_path)
